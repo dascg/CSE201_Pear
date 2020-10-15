f359fc4acf4caa515d2861f7c410683c7de786b3
SauceyDragon <58600892+SauceyDragon@users.noreply.github.com>
2020-10-15 11:47:49 -0400

    Add files via upload

--- Guest.java
+++ Guest.java
@@ -1,17 +1,6 @@
 
 public class Guest implements Person{
 
-	@Override
-	public boolean view(Application a) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void sort() {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
 	public int checkPrivillege() {
@@ -19,11 +8,6 @@ public class Guest implements Person{
 		return 0;
 	}
 
-	@Override
-	public void filter() {
-		// TODO Auto-generated method stub
-		
-	}
-	
+
 
 }
---
+++ Store.java
@@ -0,0 +1,25 @@
+
+public class Store {
+	// List of Users
+	// ArrayList of Applications
+	
+
+	public boolean view(Application a) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public void sort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void filter() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
+}
