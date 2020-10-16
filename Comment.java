fd78826ff7c1146f87e33568ff1effaef749fcb4
Jorge <53016661+jorgenadjar@users.noreply.github.com>
2020-10-16 00:48:07 -0400

    Added constructor and methods to Comment class

--- Comment.java
+++ Comment.java
@@ -1,4 +1,20 @@
 
 public class Comment {
+  
+  private int id;
+  private String timestamp;
+  private User user;
+  
+  public Comment(int id, String timestamp, User user) {
+    this.id = id;
+    this.timestamp = timestamp;
+    this.user = user;
+  }
+  
+  public boolean edit(String str) { }
+  
+  public boolean delete() { }
+  
+  public boolean flag(int id) { }
 
 }
