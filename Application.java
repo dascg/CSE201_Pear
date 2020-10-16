e388e2a0d7fbd30fa40313b54303148ed726f5d6
Jorge <53016661+jorgenadjar@users.noreply.github.com>
2020-10-16 00:41:21 -0400

    Added getters and setters to the Application class

--- Application.java
+++ Application.java
@@ -1,18 +1,79 @@
 
 public class Application {
-	
-	
-	private String name;
-	private String description;
-	private String orgnization;
-	private String platform;
-	private String version;
-	private String link;
-	private double price;
-	
-	public Application() {
 		
-	}
-	
+    private String name;
+    private String description;
+    private String organization;
+    private String platform;
+    private String version;
+    private String link;
+    private double price;
+
+    public Application(String name, String description, String organization,
+                       String platform, String version, String link, double price) {
+        this.name = name;
+        this.description = description;
+        this.organization = organization;
+        this.platform = platform;
+        this.version = version;
+        this.link = link;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 }
