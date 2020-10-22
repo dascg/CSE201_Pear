9d0df197bc2538750636bf5f81205ca487c40247
dascg <72758056+dascg@users.noreply.github.com>
2020-10-22 11:58:46 -0400

    Update User.java

--- User.java
+++ User.java
@@ -4,18 +4,18 @@ public class User extends Guest {
 	public String username;
 	public String password;
 	public String email;
-	
+	//User constructor
 	public User(String username, String password, String email) {
 		this.username = username;
 		this.password = password;
 		this.email = email;
 		this.ID = 1;
 	}
-	
+	//request to view application
 	public boolean requestForm(Application a) {
 		return false;
 	}
-	
+	//method for leaving a comment
 	public boolean comment() {
 		return false;
 	}
